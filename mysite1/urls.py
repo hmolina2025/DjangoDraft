0b6a3ee1aafea03c805b346dfa362eb21f82db5e
hmolina2025 <73655510+hmolina2025@users.noreply.github.com>
2020-11-09 12:17:44 -0500

    Add files via upload

---
+++ mysite1/urls.py
@@ -0,0 +1,7 @@
+from django.conf.urls import url
+from . import views
+from dhango.contrib.auth.views import login
+
+urlpatterns = [
+    url(r'^$', views.home),
+    url(r'^login/$', login, {'template_name': 'accounts/login.html'})
